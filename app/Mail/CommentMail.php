bf45cdb3f44ebb091a7995e0b02851f825a63474
rustem-yam <yamaltdinov.rustem2004@yandex.ru>
2024-01-12 03:32:45 +0300

    first commit

---
+++ app/Mail/CommentMail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Comment;
+
+class CommentMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+
+     protected $text_article;
+     protected $comment;
+
+    public function __construct(Comment $comment, string $text_article)
+    {
+        $this->text_article = $text_article;
+        $this->comment = $comment;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(env('MAIL_USERNAME'), 'Admin')
+                    ->subject('Тема')
+                    ->view('mail.comment')
+                    ->with([
+                        'text_article' => $this->text_article,
+                        'comment' => $this->comment
+                    ]);
+    }
+}
